e977be7413a862026a86730059caeb53a18a1cb9
beerit00 <beerit00@hs-esslingen.de>
2020-12-22 19:21:48 +0100

    Still Debuging w2g function for Ubuntu Server

--- w2g.py
+++ w2g.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(driver_path='/home/ubuntu/github/Discordbot', chrome_options=chrome_options, service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
+driver = webdriver.Chrome(executable_path='/home/ubuntu/github/Discordbot/chromedriver', chrome_options=chrome_options, service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
 driver.get("http://www.w2g.tv")
 time.sleep(2)
 
