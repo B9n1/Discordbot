b7f12736ef0407d832cb562bb8e272c1d5ffd75b
beerit00 <beerit00@hs-esslingen.de>
2020-12-22 19:34:42 +0100

    Still Debuging w2g function for Ubuntu Server

--- w2g.py
+++ w2g.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 chrome_options = Options()
 chrome_options.headless = True
-driver = webdriver.Chrome(executable_path='/home/ubuntu/github/Discordbot/chromedriver',options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("http://www.w2g.tv")
 time.sleep(2)
 
